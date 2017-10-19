7957e7e5b618e4c04dbb54c6bb9aed2b34fe5d8b
mariam abdelhamid <mariam.abdelhamid@v-apps.co>
2017-10-19 23:05:59 +0200

    constant, utilities and athletes view

---
+++ Athletes/Athletes/SupportClasses/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Athletes
+//
+//  Created by mariam abdelhamid on 10/19/17.
+//  Copyright © 2017 Mariam Abdelhamid. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+    //MARK: Api Urls
+    internal static let getAthletesUrl = "https://gist.githubusercontent.com/Bassem-Samy/f227855df4d197d3737c304e9377c4d4/raw/ece2a30b16a77ee58091886bf6d3445946e10a23/athletes.josn"
+}
